cbd20e4d45627b09b9b34384b601e61bb462f2d4
NyrekWheeler <142206465+Nyrek2001@users.noreply.github.com>
2025-03-20 13:05:56 -0400

    Update add_stack.rs

--- src/add_stack.rs
+++ src/add_stack.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+// Function to add two numbers allocated on the stack
+pub fn add_stack(a: i32, b: i32) -> i32 {
+    a + b
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_stack() {
+        assert_eq!(add_stack(5, 10), 15);
+        assert_eq!(add_stack(-5, 5), 0);
+    }
 }
